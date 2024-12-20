0ea548c3c3fbfce4b8f877a37c32044bce3d7b81
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-12-20 12:54:38 +0100

    radar chart for stats

---
+++ components/Details/Stats.js
@@ -0,0 +1,49 @@
+import { FlatList, StyleSheet, Text, View} from "react-native";
+import tw from "twrnc";
+import { capitalizeFirstChar } from "../../utils/utils";
+import { RadarChart } from "@salmonco/react-native-radar-chart";
+
+
+
+export default function Stats({stats}) {
+
+    const statsData = stats.map((stat) => {
+        return {
+            label: capitalizeFirstChar(stat.stat.name),
+            value: stat.base_stat,
+        };
+    }, [stats]);
+
+    return (
+    <View style={styles.container}>
+        <Text style={styles.detailsTitle}>Stats:</Text>
+        <RadarChart
+            data={statsData}
+            size={window.width}
+            scale={0.95}
+            maxValue={150}
+            gradientColor={{
+            startColor: 'grey',
+            endColor: 'lightgrey',
+            count: 5,
+            }}
+            stroke={['#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#000000']}
+            strokeWidth={1}
+            labelSize={14}
+            labelDistance={1.2}
+            dataFillColor="transparent"
+            dataStroke="#b91c1c"
+            dataStrokeWidth={2}
+            isCircle
+        />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    detailsTitle: tw`text-black text-center text-lg font-bold`,
+  });
